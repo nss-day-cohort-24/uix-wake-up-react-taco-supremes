0714e1a41b020f4766b11a13004432639098aa43
Million Yemane <wedi_ngus@yahoo.com>
2018-04-16 15:43:03 -0500

    adding weather files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Weather from './Weather';
+// import {Button} from 'reactstrap';
 class App extends Component {
   render() {
     return (
@@ -10,9 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Taco Supremes</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+        <Weather/> 
+       
       </div>
     );
   }
